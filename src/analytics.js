6429805c7f2a950002c697c253b35a2d50564168
Kobzarko <evgewi4@gmail.com>
2021-04-19 14:34:18 +0300

    plugins HTML CSS loaders clean dist JSON added

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,8 @@
 function createAnalytics() {
   let counter = 0;
-  let isDestroyed = false;
+  let destroyed = false;
 
+  console.log("test");
   const listener = () => counter++;
 
   document.addEventListener("click", listener);
@@ -9,11 +10,11 @@ function createAnalytics() {
   return {
     destroy() {
       document.removeEventListener("click", listener);
-      isDestroyed = true;
+      destroyed = true;
     },
 
     getClicks() {
-      if (isDestroyed) {
+      if (destroyed) {
         return `Analytics is destroyed. Total clicks = ${counter}`;
       }
       return counter;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
+  context: path.resolve(__dirname, "src"),
   mode: "development",
   entry: {
-    main: "./src/index.js",
-    analytics: "./src/analytics.js",
+    main: "./index.js",
+    analytics: "./analytics.js",
   },
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Webpack Kobzarko",
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
