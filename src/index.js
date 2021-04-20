ee7a183b6d8842005943138bab73715b65c195b6
Kobzarko <evgewi4@gmail.com>
2021-04-20 13:15:59 +0300

    csv, png, xml, normalize loaders alias extensions added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import Post from "./Post";
-import json from "./assets/json";
+import * as $ from "jquery";
+import Post from "@models/Post";
+import json from "@/assets/json";
+import xml from "./assets/data.xml";
+import csv from "./assets/data.csv";
 import "./styles/styles.css";
-
-const post = new Post("Webpack Post Title");
+import WebpackLogo from "./assets/webpack-logo.png";
+const post = new Post("Webpack Post Title", WebpackLogo);
 
 console.log("Post to string: ", post.toString());
 
 console.log("JSON: ", json);
+console.log("XML: ", xml);
+console.log("CSV: ", csv);
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  resolve: {
+    extensions: [".js", ".json", ".png"],
+    alias: {
+      "@models": path.resolve(__dirname, "src/models"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [
     new HTMLWebpackPlugin({
       title: "Webpack Kobzarko",
@@ -24,6 +31,22 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eots)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
+      {
+        test: /\.csv$/,
+        use: ["csv-loader"],
       },
     ],
   },
